670c07f6fe92b9ada18099834ab4d99c005cec17
zhuob <zhuob@onid.oregonstate.edu>
2015-12-22 09:20:38 -0800

    polish code

--- R/R/Run.GLMM.R
+++ R/R/Run.GLMM.R
@@ -1,11 +1,14 @@
 ###  run variance component analysis for multiple- tissue, leaf and seedling data
 
 
+CodeNeeded <- paste(CodePath, "estimate.var.component.R", sep="")
+source(CodeNeeded)
+
 dataname <- c("tissue", "leaf", "seedling")
 
 for ( i in 1:length(dataname)){
   
-  dataobj <- paste(dataPath, dataname[i], ".rds", sep="")
+  dataobj <- paste(DataPath, dataname[i], ".rds", sep="")
   obj <- readRDS(dataobj)
   niters = 1
 
@@ -16,7 +19,7 @@ niter = niters,                                   # number of iteration for DESe
 filter.factor= 3,                                 # average row means below which the genes are removed, default is 3.
 topgene = 1000                                    # number of top stably expressed genes to be used for normalization, default is 1000
 )
-save.target <- paste(resultPath, data[i], ".columbia.use.reference.iter.", niters, ".rds", sep="")
+save.target <- paste(ResultPath, data[i], ".columbia.use.reference.iter.", niters, ".rds", sep="")
 saveRDS(var.obj, save.target)
 }
 
